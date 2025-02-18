98a00c941301cb63bcc2fae1389ef88b4bac862c
nurkadyr <nurkadyrurmatbekov@gmail.com>
2025-02-18 18:29:57 +0600

    first

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ from pymongo import MongoClient
 from mongo import insert_photo, insert_html_data, insert_screenshot, update_unique_status
 from ms import insert_product, insert_product_files, get_connection, is_url_exists
 
-BATCH_SIZE = 3
+BATCH_SIZE = 5
 MAX_QUEUE_SIZE = 70
-MAX_WORKERS = 36
+MAX_WORKERS = 18
 
 
 async def parse_url(urls, proxy_url, db_html, db_photos, db_screenshots, conn):
